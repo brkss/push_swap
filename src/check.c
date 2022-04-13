23074577e077bc3fe2937881b8b300561e9c1383
brkss <berkassebrahim@gmail.com>
2022-04-13 17:50:57 +0000

    🍈 check.c :  norminette

--- src/check.c
+++ src/check.c
@@ -6,42 +6,42 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:42:29 by bberkass          #+#    #+#             */
-/*   Updated: 2022/04/13 17:42:32 by bberkass         ###   ########.fr       */
+/*   Updated: 2022/04/13 17:50:23 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-static int is_alphanum(char c){
 
-
-  if((c >= '0' && c <= '9') || c == '-')
-    return (1);
-  return (0);
-
+static int	is_alphanum(char c)
+{
+	if ((c >= '0' && c <= '9') || c == '-')
+		return (1);
+	return (0);
 }
 
-static int is_arg_alphanum(char *arg){
-
-  if(!arg)
-    return (0);
-
-  while(*arg){
-    if(!is_alphanum(*arg))
-      return (0);
-    arg++;
-  }
-  return (1);
+static int	is_arg_alphanum(char *arg)
+{
+	if (!arg)
+		return (0);
+	while (*arg)
+	{
+		if (!is_alphanum(*arg))
+			return (0);
+		arg++;
+	}
+	return (1);
 }
 
-int check_parameters(int count, char **args){
+int	check_parameters(int count, char **args)
+{
+	int	i;
 
-  int i;
-
-  i = 1;
-  while(i < count && args[i]){
-    if(!is_arg_alphanum(args[i]))
-      return (0);
-    i++;
-  }
-  return (1);
+	i = 1;
+	while (i < count && args[i])
+	{
+		if (!is_arg_alphanum(args[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
