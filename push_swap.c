3c0b9be53404bdcc99cea721de560a2b13ef9f65
brkss <berkassebrahim@gmail.com>
2022-04-13 20:05:50 +0000

    🤝 main : norminette !

--- push_swap.c
+++ push_swap.c
@@ -6,45 +6,34 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:28:08 by bberkass          #+#    #+#             */
-/*   Updated: 2022/04/13 17:28:10 by bberkass         ###   ########.fr       */
+/*   Updated: 2022/04/13 20:05:37 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-
 #include "./includes/push_swap.h"
 
-int main(int argc, char ** argv){
+int	main(int argc, char **argv)
+{
+	t_stack	*a;
+	t_stack	*b;
 
-
-  t_stack  *a;
-  t_stack  *b;
-
-  if(argc > 1){
-    
-    if(!check_parameters(argc, argv)){
-      printf("\nError");
-      exit(0);
-    }
-    
-    a = (t_stack *)malloc(sizeof(t_stack));
-    b = (t_stack *)malloc(sizeof(t_stack));
-    a->stack = (int *)malloc(sizeof(int) * argc - 1);
-    b->stack = (int *)malloc(sizeof(int) * argc - 1);
-    a->stack_size = argc - 1;
-    b->stack_size = 0;
-    
-    fill_stack(a, argc, argv);
-    a->metadata.sorted = sort(a);
-    is_sorted(a); 
-
-    print_stacks(a, b);
-    get_case(a, b);
-    print_stacks(a, b);
-    
-
-    //printf("NOT SORTED !");
-    return (0);
-  }
-
+	if (argc > 1)
+	{
+		if (!check_parameters(argc, argv))
+		{
+			put_str("\nError");
+			exit(0);
+		}
+		a = (t_stack *)malloc(sizeof(t_stack));
+		b = (t_stack *)malloc(sizeof(t_stack));
+		a->stack = (int *)malloc(sizeof(int) * argc - 1);
+		b->stack = (int *)malloc(sizeof(int) * argc - 1);
+		a->stack_size = argc - 1;
+		b->stack_size = 0;
+		fill_stack(a, argc, argv);
+		a->metadata.sorted = sort(a);
+		is_sorted(a);
+		get_case(a, b);
+		return (0);
+	}
 }
--- src/print.c
+++ src/print.c
@@ -6,7 +6,7 @@
 /*   By: bberkass <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 17:43:24 by bberkass          #+#    #+#             */
-/*   Updated: 2022/04/13 17:43:26 by bberkass         ###   ########.fr       */
+/*   Updated: 2022/04/13 20:03:15 by bberkass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,41 +16,41 @@
 void print_stacks(t_stack *a, t_stack *b)
 {
 
-  int i;
-  int j;
-  
-  i = 0;
-  j = 0;
-  if(0)
-  {
+	int i;
+	int j;
 
-  printf("\n --------------------------- \n");
-  while(i < a->stack_size || j < b->stack_size)
-  {
-    if(i < a->stack_size){
-      printf("%d ", a->stack[i]);
-    }
-    printf("\t");
-    if(j < b->stack_size){
-      printf(" %d", b->stack[j]);
-    }
-    printf("\n");
-    i++;
-    j++;
-  }
+	i = 0;
+	j = 0;
+	if(0)
+	{
 
-  printf("-- \t -- \n");
-  printf("a  \t b  \n");
-  printf("\n --------------------------- \n");
-  }
+		printf("\n --------------------------- \n");
+		while(i < a->stack_size || j < b->stack_size)
+		{
+			if(i < a->stack_size){
+				printf("%d ", a->stack[i]);
+			}
+			printf("\t");
+			if(j < b->stack_size){
+				printf(" %d", b->stack[j]);
+			}
+			printf("\n");
+			i++;
+			j++;
+		}
+
+		printf("-- \t -- \n");
+		printf("a  \t b  \n");
+		printf("\n --------------------------- \n");
+	}
 }
 
 void plot_metadata(t_stack *a)
 {
-  printf("MIDDLE : %d\n", a->metadata.middle);
-  printf("END : %d\n", a->metadata.end);
-  printf("START : %d\n", a->metadata.start);
-  printf("INIT_SIZE : %d\n", a->metadata.init_size);
-  printf("OFFSET : %d\n", a->metadata.offset);
-  printf("DAWN : %d\n", a->metadata.dawn);
+	printf("MIDDLE : %d\n", a->metadata.middle);
+	printf("END : %d\n", a->metadata.end);
+	printf("START : %d\n", a->metadata.start);
+	printf("INIT_SIZE : %d\n", a->metadata.init_size);
+	printf("OFFSET : %d\n", a->metadata.offset);
+	printf("DAWN : %d\n", a->metadata.dawn);
 }
